4b511b6e2abdbafe321c7c52e4b960a7947c780e
Vinoth Joshua <vinothrajux@gmail.com>
2017-04-03 22:37:46 +0530

    Login Screen creation Dashboard sample screen creation

---
+++ app/src/main/java/com/example/vinothjoshua/siv/DashboardActivity.java
@@ -0,0 +1,17 @@
+package com.example.vinothjoshua.siv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Vinoth Joshua on 03-Apr-17.
+ */
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
--- app/src/main/java/com/example/vinothjoshua/siv/LoginActivity.java
+++ app/src/main/java/com/example/vinothjoshua/siv/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.example.vinothjoshua.siv;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //hide Title Bar
+        getSupportActionBar().hide();
+
+
+
+    }
+
+    public void dashboard(View view){
+        Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
+        startActivity(intent);
     }
 }
