95d1d34f45379a284056d6123a07c04189050d79
Seetha <rajula09mca@gmail.com>
2017-04-08 15:14:56 +0530

    activity_stores.xml

---
+++ app/src/main/java/com/example/vinothjoshua/siv/StoresActivity.java
@@ -0,0 +1,24 @@
+package com.example.vinothjoshua.siv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Seetha on 08-Apr-17.
+ */
+
+public class StoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stores);
+
+        //hide Title Bar
+        getSupportActionBar().hide();
+
+
+
+    } {
+}
