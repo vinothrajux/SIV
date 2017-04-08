050e7c65589052a819cc58d53067d23aeadc21d8
Seetha <rajula09mca@gmail.com>
2017-04-08 14:54:28 +0530

    activity_stores.xml

---
+++ app/src/main/java/com/example/vinothjoshua/siv/Stores.java
@@ -0,0 +1,20 @@
+package com.example.vinothjoshua.siv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Seetha on 08-Apr-17.
+ */
+
+public class Stores extends AppCompatActivity
+
+    {
+        @Override
+        protected void onCreate (Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.acitivity_stores)
+        }
+    }
+}
