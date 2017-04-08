39aca2e4366c09894a8d28f6bcdc650b5fb6f4fb
Seetha <rajula09mca@gmail.com>
2017-04-08 15:46:20 +0530

    activity_transport.xml

---
+++ app/src/main/java/com/example/vinothjoshua/siv/TransportActivity.java
@@ -0,0 +1,24 @@
+package com.example.vinothjoshua.siv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Seetha on 08-Apr-17.
+ */
+
+public class TransportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transport);
+
+        //hide Title Bar
+       // getSupportActionBar().hide();
+
+
+
+    }
+}
