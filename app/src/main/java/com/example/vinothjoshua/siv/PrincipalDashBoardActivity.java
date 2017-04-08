73f959c75cc1bb664a7eacadb25af2743961bfe1
Seetha <rajula09mca@gmail.com>
2017-04-08 16:28:33 +0530

    activity_principaldashboard.xml

---
+++ app/src/main/java/com/example/vinothjoshua/siv/PrincipalDashBoardActivity.java
@@ -0,0 +1,17 @@
+package com.example.vinothjoshua.siv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Seetha on 08-Apr-17.
+ */
+
+public class PrincipalDashBoardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principaldashboard);
+    }
+
+}
