c84b95135c661c494e1c7735c4a3b6326d72d857
GBCorp <informkamalakannan@gmail.com>
2017-04-09 14:09:41 +0530

    New activity added - Fee counter activity

---
+++ app/src/main/java/com/example/vinothjoshua/siv/PlacementActivity.java
@@ -0,0 +1,16 @@
+package com.example.vinothjoshua.siv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by GBCorp on 4/9/2017.
+ */
+
+public class PlacementActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_placement);
+    }
+}
