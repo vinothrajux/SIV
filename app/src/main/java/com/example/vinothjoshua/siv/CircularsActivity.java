ab2a5e1aaa86f98460d81f6517bb4942fba78282
Vinoth Joshua <vinothrajux@gmail.com>
2017-05-08 15:34:14 +0530

    merge fix

---
+++ app/src/main/java/com/example/vinothjoshua/siv/CircularsActivity.java
@@ -0,0 +1,19 @@
+package com.example.vinothjoshua.siv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by GBCorp on 4/9/2017.
+ */
+
+public class CircularsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circulars);
+    }
+
+
+}
